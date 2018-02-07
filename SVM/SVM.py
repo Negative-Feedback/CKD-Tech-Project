4a0c4abdb4005ef0e877c8d3abb47b5d86eda13b
Negative-Feedback <matthew.giorno@sympatico.ca>
2018-02-07 16:42:03 -0500

    Got conversion from arff to useable numpy array working. Need to comment and clean a little

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -2,22 +2,39 @@ import arff as larff
 import numpy as np
 from scipy.io import arff
 from sklearn import svm
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-
-raw_data, meta = arff.loadarff('C:/Users/Matthew/PycharmProjects/CKD-Tech-Project/chronic_kidney_disease.arff')
-target = raw_data[meta.names()[-1:]]  #just the last column
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import Imputer
 
 dataset = larff.load(open('C:/Users/Matthew/PycharmProjects/CKD-Tech-Project/chronic_kidney_disease.arff'))
-data = np.array(dataset['data'])
 
-data = np.delete(data, -1, axis=1)
+raw_data = np.array(dataset['data'])
+
+
+data = raw_data[:, :-1]
+target = raw_data[:, -1]
+
+#data = np.delete(data, -1, axis=1)
+
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imp.fit(data)
+
+data = imp.fit_transform(data)
+
+#enc.fit(data)
+#enc.fit(target)
+
+
 
 #print(data.shape, target.shape)
 #print(data)
+#print(target)
 
-target = np.asarray(target)
+
 
 data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3)
+
 
 clf = svm.SVC()
 clf.fit(data_train, target_train)
