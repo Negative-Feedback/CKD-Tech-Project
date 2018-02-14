3f6e3833c2bed847ceb4e7e467ec2b1d85a957cb
TUnveiled <32398411+TUnveiled@users.noreply.github.com>
2018-02-14 17:52:18 -0500

    better organized brute force optimization

--- NN/OptimizationWith2Layers.py
+++ NN/OptimizationWith2Layers.py
@@ -3,18 +3,30 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Imputer
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import f1_score
 
+# open the arff file
 dataset = arff.load(open('ckd.arff'))
-raw_data = np.array(dataset['data']) # pulls the data out into a numpy array
 
-data = raw_data[:, :-1] # takes everything except the last column
-target = raw_data[:, -1] # just the last column
+# pulls the data into a numpy array
+raw_data = np.array(dataset['data'])
 
-imp = Imputer(missing_values='NaN', strategy='mean', axis=0) #fixes missing data by taking values from other rows and taking the average
-imp.fit(data) #iirc this fucntion takes the average
-data = imp.fit_transform(data) #inserts the average into the missing spots
+# takes everything except the last column
+data = raw_data[:, :-1]
 
+# just the last column
+target = raw_data[:, -1]
 
+# fixes missing data by taking values from other rows and taking the average
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+
+# this function takes the average of every column excluding the unknown values
+imp.fit(data)
+
+# inserts the average into the missing spots
+data = imp.fit_transform(data)
+
+# Function that creates the neural network 100 times and takes the average of its F1 score
 def aveaccuracy(data, target, h1, h2):
     toreturn = 0.
     for n in range(100):
@@ -22,17 +34,16 @@ def aveaccuracy(data, target, h1, h2):
         clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(h1, h2), random_state=1)
         clf.fit(data_train, target_train)
         prediction = clf.predict(data_test)
-        accuracy = 0.
-        for n in range(target_test.size):
-            if target_test[n] == prediction[n]:
-                accuracy += 1.
-        accuracy /= target_test.size
+        accuracy = f1_score(target_test, prediction, pos_label='1')
         toreturn += accuracy
     return toreturn
 
 
+# default values
 ideal = [0, 0]
 maxi = 0
+
+# check a lot of hidden layer configurations for sets with high accuracy
 for x in range(2, 25):
     for y in range(1, x):
         temp = aveaccuracy(data, target, x, y)
@@ -42,4 +53,5 @@ for x in range(2, 25):
         if temp > 84:
             print("The predictions were " + str(temp) + "% accurate on average for " + str([x, y]))
 
+# print the highest accuracy one
 print(str(ideal) + " gives " + str(maxi) + "% accuracy")
