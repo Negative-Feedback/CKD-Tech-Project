50467bee74b3f1f31672d67deb6bd2d29ed0f02c
KevinGenereux <kwgenere@lakeheadu.ca>
2018-03-04 14:25:59 -0500

    Testing Itis dataset

--- KNN/NearestNeighbour.py
+++ KNN/NearestNeighbour.py
@@ -21,57 +21,7 @@ data = imp.fit_transform(data) #inserts the average into the missing spots
 data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3) #breaks the dataset into test and training data
 #30% of data is test data
 
-def EuclideanDistance(instance1, instance2, length):
-    distance = 0
-    for x in range(length):
-        distance += pow((instance1[x] - instance2[x]), 2)
-    return math.sqrt(distance)
-
-def getNeighbors(trainingSet, testInstance, k):
-    distances = []
-    length = len(testInstance)-1
-    for x in range(len(trainingSet)):
-        dist = EuclideanDistance(testInstance, trainingSet[x], length)
-        distances.append((trainingSet[x], dist))
-    distances.sort(key=operator.itemgetter(1))
-    neighbors = []
-    for x in range(k):
-        neighbors.append(distances[x][0])
-    return neighbors
-
-def getResponse(neighbors):
-    classVotes = {}
-    for x in range(len(neighbors)):
-        response = neighbors[x][-1]
-        if response in classVotes:
-            classVotes[response] += 1
-        else:
-            classVotes[response] = 1
-    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedVotes[0][0]
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for x in range(len(testSet)):
-        if testSet[x][-1] is predictions[x]:
-            correct += 1
-    return (correct/float(len(testSet))) * 100.0
-
-def Main():
-
-    print ('Train set: ' + repr(len(data_train)))
-    print ('Test Set: ' + repr(len(data_test)))
-
-    #generate predictions
-
-    predictions=[]
-    k = 3
-    for x in range(len(data_test)):
-        neighbors = getNeighbors(data_train, data_test[x], k)
-        result = getResponse(neighbors)
-        predictions.append(result)
-        print('> predicted=' + repr(result) + ', actual=' + repr(data_train[x][-1]))
-    accuracy = getAccuracy(data_train, predictions)
-    print('Accuracy: ' + repr(accuracy) + '%')
-
-Main()
+from sklearn.datasets import load_iris
+iris = load_iris()
+type(iris)
+print(iris.data)
