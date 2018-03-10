7317f20fd598bac31c5565fb0bfe57fbeb12d722
tmcphee <32398415+tmcphee@users.noreply.github.com>
2018-03-10 13:52:26 -0500

    Organized Code

--- Prototype/CKD UI.py
+++ Prototype/CKD UI.py
@@ -10,26 +10,18 @@ root.geometry("700x400")#sets gui size
 root.resizable(0, 0)
 root.title("Chronic Kidney Disease - Prototype")
 var = IntVar()
-R1 = Radiobutton(root, text="Decision Tree", variable=var, value=1,
-                  command=sel)
+R1 = Radiobutton(root, text="Decision Tree", variable=var, value=1,command=sel)
 R1.pack( anchor = W )
 
-R2 = Radiobutton(root, text="KNN", variable=var, value=2,
-                  command=sel)
+R2 = Radiobutton(root, text="KNN", variable=var, value=2,command=sel)
 R2.pack( anchor = W )
-
-R3 = Radiobutton(root, text="LR", variable=var, value=3,
-                  command=sel)
+R3 = Radiobutton(root, text="LR", variable=var, value=3,command=sel)
 R3.pack( anchor = W )
-
-R4 = Radiobutton(root, text="NN", variable=var, value=4,
-                  command=sel)
+R4 = Radiobutton(root, text="NN", variable=var, value=4,command=sel)
 R4.pack( anchor = W)
-R5 = Radiobutton(root, text="RF", variable=var, value=5,
-                  command=sel)
+R5 = Radiobutton(root, text="RF", variable=var, value=5,command=sel)
 R5.pack( anchor = W)
-R6 = Radiobutton(root, text="SVM", variable=var, value=6,
-                  command=sel)
+R6 = Radiobutton(root, text="SVM", variable=var, value=6,command=sel)
 R6.pack( anchor = W)
 
 label = Label(root)
