bfd81a6ca8abe608306bc9c99125cd708b0ca406
TUnveiled <32398411+TUnveiled@users.noreply.github.com>
2018-03-11 15:29:55 -0400

    did some informal optimization

--- NN/OptimizingFeatureSelection.py
+++ NN/OptimizingFeatureSelection.py
@@ -11,9 +11,9 @@ maxi = 0.
 
 # find the average F1 score and its standard deviation for all the layer sizes
 print("Number of Features/tp/tn/fp/fn/f1/precision/sensitivity/specificity/accuracy")
-for x in range(4, 15):
+for x in range(8, 11):
+    data, target = metrics.preprocess(k=x, fsiter=1000, scaling=False)
     for neurons in range(1, 10*x):
-        data, target = metrics.preprocess(k=x)
         temp = metrics.repeatedCrossValidatedScores(data, target,
                                MLPClassifier(solver='lbfgs', alpha=0.001, hidden_layer_sizes=neurons, random_state=1),
                                iterations=20, cv=10)
