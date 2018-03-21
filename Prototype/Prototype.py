eb2de7c6b94111a6b6693fa15acfeb699ba67c58
Negative-Feedback <matthew.giorno@sympatico.ca>
2018-03-21 12:39:20 -0400

    added code to Prototype.py and added Results.html

--- Prototype/Prototype.py
+++ Prototype/Prototype.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask, redirect, url_for, render_template, request
 from werkzeug.utils import secure_filename
+from sklearn.externals import joblib
+import arff
+import numpy as np
 
 UPLOAD_FOLDER = '/Prototype'
-ALLOWED_EXTENSIONS = set(['arff'])
+#ALLOWED_EXTENSIONS = set(['arff'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -15,14 +18,22 @@ def Main_file():
 
 
 
-@app.route('/upload_file', methods=['GET', 'POST'])
+@app.route('/results', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.filename = "temp_upload.arff"
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        dataset = arff.load(f)
+        data = np.array(dataset['data'])
+        predection = getPredection(data)
+        predection = 0
+        return render_template('Results.html', result = predection)
 
+
+
+def getPredection(data):
+    MLA = joblib.load('SVM/SVM.pkl')
+    result = MLA.predict(data)
+    return(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
